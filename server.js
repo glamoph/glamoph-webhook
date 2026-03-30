39037fffdd1260e35ba0679f4965f627d0ad8d17
glamoph <glamoph@gmail.com>
2026-03-30 17:40:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const { syncProductFeaturedImageToGitHub } = require("./lib/image-sync");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 function escapeHtml(value) {
   return String(value ?? "")
     .replace(/&/g, "&amp;")
