b3a2e2366f627c0bea64663c522bed429e8da0dd
glamoph <glamoph@gmail.com>
2026-04-30 13:45:08 +0900

    Update server.js

--- server.js
+++ server.js
@@ -650,7 +650,9 @@ async function createRecordFile(internalId, record) {
   );
 
   const staticHtml = buildPageHtml(record, record.archiveId, { isOwner: false })
-    .replace('href="/archive.css"', `href="${ARCHIVE_ASSET_BASE_URL}/archive.css"`)
+    const staticHtml = buildPageHtml(record, record.archiveId, { isOwner: false })
+  .replace('href="/archive.css"', `href="${ARCHIVE_ASSET_BASE_URL}/public/archive.css"`)
+  .replace('src="/assets/signature.png"', `src="${ARCHIVE_ASSET_BASE_URL}/public/assets/signature.png"`);
     .replace('src="/assets/signature.png"', `src="${ARCHIVE_ASSET_BASE_URL}/assets/signature.png"`);
 
   await putFileBase64({
